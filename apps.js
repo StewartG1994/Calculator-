5bd6b11b11fea1661ce4e2e5f7432d5dbf41a240
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-02-13 21:26:09 +0000

    Working Calculator - Operation chaining bug fixed

--- apps.js
+++ apps.js
@@ -7,86 +7,86 @@ const equalsBtn = document.querySelector('.equals');
 
 
 button.forEach((button) =>
-button.addEventListener('click', ()=> displayTotal(button.innerHTML)));
+  button.addEventListener("click", () => displayTotal(button.innerHTML)) 
+);
 
-clearBtn.addEventListener('click', clear);
+clearBtn.addEventListener("click", clear);
 
-decmialBtn.addEventListener('click', appendDecimal);
+decmialBtn.addEventListener("click", appendDecimal);
 
-functionBtn.forEach((button) => 
-button.addEventListener('click', () => setOperator(button.innerHTML)));
+functionBtn.forEach((button) =>
+  button.addEventListener("click", () => setOperator(button.innerHTML)) 
+);
 
-equalsBtn.addEventListener('click', calculate)
+equalsBtn.addEventListener("click", calculate);
 
-let firstNum = 0;                     
-let secondNum = 0;
-let operatorSelection = "";
+let firstNum = "";
+let secondNum = "";
+let operatorSelection = null;
+let result = "";
 
-function add(a, b){
-    return a + b
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a,b){
-return a - b
+function subtract(a, b) {
+  return a - b;
 }
 
-function divide (a,b)
-{
-return a / b
+function divide(a, b) {
+  return a / b;
 }
 
-function times (a,b){
-return a*b
+function times(a, b) {
+  return a * b;
 }
-
 
 function operator(operator, numa, numb) {
-     switch(operator){
-         case '+':
-             return add(numa, numb);
-             break;
+  switch (operator) {
+    case "+":
+      return add(numa, numb);
+      break;
 
-         case '-':
-             return subtract(numa, numb);
-             break;
-        
-        case '/':
-             return divide(numa,numb);
-             break;
+    case "-":
+      return subtract(numa, numb);
+      break;
 
-        case '*':
-            return times(numa,numb)
-            break;
-     }
+    case "/":
+      return divide(numa, numb);
+      break;
+
+    case "*":
+      return times(numa, numb);
+      break;
+  }
 }
 
-function displayTotal (number){
-userDisplay.value += number;
-
+function displayTotal(number) {
+  userDisplay.value += number;
+  
 }
 
-function clear (){
-    userDisplay.value = "";
+function clear() {
+  userDisplay.value = null;
 }
 
-function appendDecimal(){
-    userDisplay.value += ".";
+function appendDecimal() {
+  userDisplay.value += ".";
 }
 
-
-function setOperator(operator){
-    firstNum = parseInt(userDisplay.value);
-    operatorSelection = operator;
-    clear()
-     }
-
-
-     
-function calculate(){
-    for (i = 0; i < 100; i++)
-    secondNum = parseInt(userDisplay.value);
-    result = operator(operatorSelection, firstNum, secondNum);
-    userDisplay.value = result.toString().substr(0,16);
-    secondNum = parseInt(userDisplay.value);
-    console.log(operatorSelection,firstNum,secondNum);
+function setOperator(operator) {
+  if (operatorSelection !== null)
+    calculate()
+  firstNum = parseFloat(userDisplay.value);
+  operatorSelection = operator;
+  userDisplay.value = firstNum;
+  clear();
 }
+
+function calculate() {
+  
+  secondNum = parseFloat(userDisplay.value);
+  result = operator(operatorSelection, firstNum, secondNum);
+  userDisplay.value = result.toString().substr(0,8);
+  operatorSelection = null;
+} 
