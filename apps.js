c8766e5bd963e404c98056f9d6be4e5753021a19
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-09-07 15:41:25 +0100

    update

--- apps.js
+++ apps.js
@@ -1,3 +1,14 @@
+const operatorButton = document.querySelectorAll('.function')
+const userDisplay = document.querySelector(".screen");
+const clearBtn = document.querySelector('.clear');
+const numberButton = document.querySelectorAll('.number');
+const decmialBtn = document.querySelector('.decimal');
+const history = document.querySelector('.history')
+
+numberButton.forEach((numberButton) => numberButton.addEventListener('click', () => displayTotal(numberButton.value)))
+clearBtn.addEventListener('click', () => { userDisplay.value = null})
+
+
 function add(a,b){return a +b};
 function subtract(a,b){return a-b};
 function divide (a,b){return a/b}
@@ -20,7 +31,14 @@ function calclate(operator, numberOne, numberTwo){
     case '*':
     return multiply(numberOne, numberTwo);
     break;
-    
-
   }
 }
+
+function displayTotal(number){
+  userDisplay.value += number;
+}
+
+function getFirstNum(){
+  operatorButton.forEach((operatorButton) => operatorButton.addEventListener('click', () => userDisplay.value  += operatorButton.value))
+}
+getFirstNum()
