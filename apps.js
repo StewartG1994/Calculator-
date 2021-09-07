6f2ba9689a710ad5435868cb240320bf0964c813
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-09-07 23:06:50 +0100

    Working calc

--- apps.js
+++ apps.js
@@ -3,18 +3,24 @@ const userDisplay = document.querySelector(".screen");
 const clearBtn = document.querySelector('.clear');
 const numberButton = document.querySelectorAll('.number');
 const decmialBtn = document.querySelector('.decimal');
-const history = document.querySelector('.history')
+const history = document.querySelector('.history');
+const equals = document.querySelector('.equals');
+
+let firstNum = null;
+let secondNum = null;
+let operatorSelected =null;
+let historyArray = null;
 
 numberButton.forEach((numberButton) => numberButton.addEventListener('click', () => displayTotal(numberButton.value)))
-clearBtn.addEventListener('click', () => { userDisplay.value = null})
-
+clearBtn.addEventListener('click', () => { userDisplay.value = null; history.value = null})
+decmialBtn.addEventListener('click', appendDecimal)
 
 function add(a,b){return a +b};
 function subtract(a,b){return a-b};
 function divide (a,b){return a/b}
-function multiply (a,b){return a*b}
+function multiply (a,b){return a*b} 
 
-function calclate(operator, numberOne, numberTwo){
+function calculate(operator, numberOne, numberTwo){
   switch (operator) {
    case '+': 
    return add(numberOne, numberTwo);
@@ -38,7 +44,21 @@ function displayTotal(number){
   userDisplay.value += number;
 }
 
-function getFirstNum(){
-  operatorButton.forEach((operatorButton) => operatorButton.addEventListener('click', () => userDisplay.value  += operatorButton.value))
+function appendDecimal(){
+ userDisplay.value += '.';
+
 }
-getFirstNum()
+function getFirstNumAndOperator(){
+  operatorButton.forEach((operatorButton) => operatorButton.addEventListener('click', () =>{firstNum = userDisplay.value ; history.value  = userDisplay.value += operatorButton.value ; operatorSelected = operatorButton.value; userDisplay.value = '' }))
+}
+getFirstNumAndOperator()
+
+function executeCalc (){
+equals.addEventListener('click', () =>{
+  secondNum = userDisplay.value;
+
+  let result = calculate(operatorSelected, firstNum, secondNum)
+  userDisplay.value = result;
+})
+}
+executeCalc ()
